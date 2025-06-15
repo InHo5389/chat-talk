458792488cf0179dcabcfc8e2fd8a9c3b5cdc2a7
InHo <qheogus55@naver.com>
2025-06-16 02:50:26 +0900

    [#1]feat: 회원 인증,회원가입, 로그인 구현

---
+++ src/main/java/chatserver/member/dto/MemberLoginDto.java
@@ -0,0 +1,14 @@
+package chatserver.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginDto {
+
+    private String email;
+    private String password;
+}
--- src/main/java/chatserver/member/service/MemberService.java
+++ src/main/java/chatserver/member/service/MemberService.java
@@ -3,12 +3,20 @@ package chatserver.member.service;
 import chatserver.member.domain.Member;
 import chatserver.member.domain.Role;
 import chatserver.member.dto.MemberJoinDto;
+import chatserver.member.dto.MemberListDto;
+import chatserver.member.dto.MemberLoginDto;
 import chatserver.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -18,6 +26,8 @@ public class MemberService {
 
     @Transactional
     public Member join(MemberJoinDto dto) {
+        log.info("MemberController join email :{}, name :{}", dto.getEmail(), dto.getName());
+
         if (memberRepository.findByEmail(dto.getEmail()).isPresent()) {
             throw new RuntimeException("이미 존재하는 이메일입니다.");
         }
@@ -29,5 +39,24 @@ public class MemberService {
                 .role(Role.USER)
                 .build());
         return saverMember;
+    }
+
+    @Transactional
+    public Member login(MemberLoginDto dto) {
+        Member member = memberRepository.findByEmail(dto.getEmail()).orElseThrow(
+                () -> new RuntimeException("존재하지 않는 회원입니다.")
+        );
+
+        if (!passwordEncoder.matches(dto.getPassword(), member.getPassword())) {
+            throw new RuntimeException("비밀번호가 틀렸습니다.");
+        }
+        return member;
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberListDto> findAll() {
+        return memberRepository.findAll().stream()
+                .map(MemberListDto::from)
+                .toList();
     }
 }
